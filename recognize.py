8e183f7bf52a612b0ac6c742b32853981376fd81
yangjc <bupt_yjc@163.com>
2018-10-27 15:40:12 +0800

    change embed

--- recognize.py
+++ recognize.py
@@ -21,8 +21,7 @@ from util import map_pos, map_item
 
 def define_nn_crf(name, embed_mat, seq_len, class_num):
     vocab_num, embed_len = embed_mat.shape
-    embed = Embedding(input_dim=vocab_num, output_dim=embed_len,
-                      weights=[embed_mat], input_length=seq_len, trainable=True)
+    embed = Embedding(input_dim=vocab_num, output_dim=embed_len, input_length=seq_len)
     input = Input(shape=(seq_len,))
     embed_input = embed(input)
     func = map_item(name, funcs)
